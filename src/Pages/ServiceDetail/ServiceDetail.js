83705cd7380973e73bf19e74cf21678f9a692c74
mozammelhoq1 <mozammelhoq512@gmail.com>
2022-04-28 06:37:05 +0600

    service load from mongoDb

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const ServiceDetail = () => {
-    const { serviceId } = useParams();
-    return (
-        <div>
-            <h2>Welcome to detail: {serviceId}</h2>
-            <div className='text-center'>
-                <Link to="/checkout">
-                    <button className='btn btn-primary'>Proceed Checkout</button>
-                </Link>
-            </div>
-        </div>
-    );
+  const { serviceId } = useParams();
+  const { service, setService } = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:5000/service/${serviceId}`)
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, [serviceId]);
+  return (
+    <div>
+      <h2>you are about to book : {serviceId}</h2>
+      <div className="text-center">
+        <Link to="/checkout">
+          <button className="btn btn-primary">Proceed Checkout</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceDetail;
